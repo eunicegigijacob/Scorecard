9dd25bfffb938f464f940234da98f23796962a04
eunicegigijacob <eunice.gigij@gmail.com>
2022-10-31 03:48:55 +0100

    Added a javascript file .
    Also made adjustment to the css by moving the centering of the page away from the body element and also added a new div that is parent element to the original container div, and moved the centering to this new div.

---
+++ index.js
@@ -0,0 +1,48 @@
+var guestPoints = 0;
+var homePoints = 0;
+
+var homePointsElement = document.getElementById("home_points")
+homePointsElement.textContent = homePoints
+
+var guestPointsElement = document.getElementById("guest_points")
+guestPointsElement.textContent = guestPoints
+
+
+function add1(points) {
+    if (points == 'home_points'){
+        homePoints += 1;
+        homePointsElement.textContent = homePoints;
+        console.log('homepoint added');
+    }else{
+        guestPoints += 1;
+        guestPointsElement.textContent = guestPoints
+        console.log('guestpoint added');
+
+    }
+}
+
+function add2(points) {
+    if (points == 'home_points'){
+        homePoints += 2;
+        homePointsElement.textContent = homePoints;
+        console.log('homepoint added');
+    }else{
+        guestPoints += 2;
+        guestPointsElement.textContent = guestPoints
+        console.log('guestpoint added');
+
+    }
+}
+
+function add3(points) {
+    if (points == 'home_points'){
+        homePoints += 3;
+        homePointsElement.textContent = homePoints;
+        console.log('homepoint added');
+    }else{
+        guestPoints += 3;
+        guestPointsElement.textContent = guestPoints
+        console.log('guestpoint added');
+
+    }
+}
